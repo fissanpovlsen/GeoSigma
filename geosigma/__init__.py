353727b572e056f15ec464beb50e29c131fded57
fissanpovlsen <rasmus_bodker@hotmail.com>
2025-11-13 14:59:57 +0100

    Introduced chol realization generator
    Function to Generate realizations from a covariance matrix using Cholesky decomposition.

--- geosigma/__init__.py
+++ geosigma/__init__.py
@@ -14,3 +14,4 @@ from .semivar_synth import semivar_synth
 from .deformat_variogram import deformat_variogram
 from .matlab_helpers import matlab_meshgrid
 from .local_kriging_setup_img import local_kriging_setup_img
+from .get_reals_cholesky import get_reals_cholesky
