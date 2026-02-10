42ed444af0cfb01819ab7fa9d9019e9efe08f169
fissanpovlsen <rasmus_bodker@hotmail.com>
2026-02-10 11:37:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="geosigma",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+)
