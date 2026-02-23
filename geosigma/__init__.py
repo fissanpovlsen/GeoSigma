33c49643bb701598783a92735e6270a24856b566
fissanpovlsen <rasmus_bodker@hotmail.com>
2026-02-23 09:25:34 +0100

    Inpox introduced

--- geosigma/__init__.py
+++ geosigma/__init__.py
@@ -12,6 +12,22 @@ from .precal_cov import precal_cov
 from .edist import edist
 from .semivar_synth import semivar_synth
 from .deformat_variogram import deformat_variogram
-from .matlab_helpers import matlab_meshgrid
 from .local_kriging_setup_img import local_kriging_setup_img
+from .least_squares_inversion import least_squares_inversion
 from .get_reals_cholesky import get_reals_cholesky
+from .inpox import draw_points_inpox,ftot_laplace,laplacian_2d
+
+
+
+__all__ = [
+    "precal_cov",
+    "edist",
+    "semivar_synth",
+    "local_kriging_setup_img",
+    "deformat_variogram",
+    "get_reals_cholesky",
+    "least_squares_inversion",
+    "draw_points_inpox",
+    "ftot_laplace",
+    "laplacian_2d"
+]
