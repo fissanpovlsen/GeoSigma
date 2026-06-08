dc5bb453adb1de13284093b959c00fa6bb913539
fissanpovlsen <rasmus_bodker@hotmail.com>
2026-06-08 09:03:13 +0200

    Script to convert ESRI Ascii to geotiff

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,10 +2,12 @@ from .load_esri_ascii_grid import load_esri_ascii_grid
 from .load_geotiff_grid import load_geotiff_grid
 from .grid_coordinates_from_esri_meta import grid_coordinates_from_esri_meta
 from .matlab_helpers import matlab_meshgrid
+from .ascii_to_geotiff import ascii_to_geotiff
 
 __all__ = [
     "load_esri_ascii_grid",
     "load_geotiff_grid.py",
     "grid_coordinates_from_esri_meta",
     "matlab_meshgrid",
+    "ascii_to_geotiff"
 ]
